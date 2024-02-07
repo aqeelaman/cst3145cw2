fce74a112c93c511b25238f7dbe1ae7742394f4a
aqeelaman <AA5226@live.mdx.ac.uk>
2024-02-07 21:14:55 +0400

    Create lessons.js

---
+++ lessons.js
@@ -0,0 +1,92 @@
+let lessons =[
+    {
+        id: 1001,
+        title: "Football",
+        location:"Al Nahda",
+        price: 400,
+        image: "images/football.png",
+        availableInventory:5,
+        cartInventory:5
+    },
+    {
+        id: 1002,
+        title: "Basketball",
+        location:"Al Barsha 2",
+        price: 200,
+        image: "images/basketball.png",
+        availableInventory:5,
+        cartInventory:5
+    },
+    {
+        id: 1003,
+        title: "Swimming",
+        location:"Karama",
+        price: 160,
+        image: "images/swimming.png",
+        availableInventory:5,        
+        cartInventory:5
+        },
+    {
+        id: 1004,
+        title: "Tennis",
+        location:"Oud Metha",
+        price: 175,
+        image: "images/tennis.png",
+        availableInventory:5,        
+        cartInventory:5
+        },
+    {
+        id: 1005,
+        title: "Badminton",
+        location:"Deira",
+        price: 210,
+        image: "images/badminton.png",
+        availableInventory:5,
+        cartInventory:5
+    },
+    {
+        id: 1006,
+        title: "Art & Craft",
+        location:"Al Qusais 2",
+        price: 340,
+        image: "images/art.png",
+        availableInventory:5,
+        cartInventory:5
+    },
+    {
+        id: 1007,
+        title: "Coding",
+        location:"Bur Dubai",
+        price: 290,
+        image: "images/coding.png",
+        availableInventory:5,
+        cartInventory:5
+    },
+    {
+        id: 1008,
+        title: "Mathematics",
+        location:"Discovery Gardens",
+        price: 360,
+        image: "images/mathematics.png",
+        availableInventory:5,
+        cartInventory:5
+    },
+    {
+        id: 1009,
+        title: "Dance",
+        location:"Mirdif",
+        price: 220,
+        image: "images/dance.png",
+        availableInventory:5,
+        cartInventory:5
+    },
+    {
+        id: 1010,
+        title: "Cricket",
+        location:"Al Warqa 2",
+        price: 320,
+        image: "images/cricket.png",
+        availableInventory:5,        
+        cartInventory:5
+        }
+]
